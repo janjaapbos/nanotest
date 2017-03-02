488fb962a120b5158282ff1842c12e159d05c01e
Janjaap Bos <janjaap.bos@capitar.com>
2017-03-02 20:23:11 +0000

    .

--- example1/example1/config.py
+++ example1/example1/config.py
@@ -3,7 +3,7 @@ If this config file is placed in the same directory as the
 frozen binary, is will be loaded instead of the frozen config.
 """
 
-ctl_url = 'ipc:///tmp/oi-ibeodwugtk.sock'
+ctl_url = 'ipc:///tmp/oi-xkailhqsjy.sock'
 
 
 def main_hook(ctx=None):
@@ -11,4 +11,12 @@ def main_hook(ctx=None):
     Custom hook to be executed. A return value other than None
     will stop further execution.
     """
-    pass
+    ctx['locals']['logging'].info('config.main_hook')
+
+def register_hook(ctx=None):
+    """
+    Custom hook to extend and register commands with the program.
+    ctx is a dict with locals, globals and program object
+    """
+    ctx['locals']['logging'].info('config.register_hook')
+    ctx['program'].add_command('hello', lambda: 'world')
